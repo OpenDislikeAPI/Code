cd760f63e5a3e383c5ae8d691fc8b6c103f195b0
hrichiksite <hrichik.site@gmail.com>
2022-01-15 18:53:38 +0530

    update a thing

--- crypto.js
+++ crypto.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const crypto = require('crypto');
-const key = require('./config/config');
-
-const ENCRYPTION_KEY = key.enc; // Must be 256 bits (32 characters)
+const config = require('./config/config');
 const IV_LENGTH = 16; // For AES, this is always 16
 
 function encrypt(text) {
  let iv = crypto.randomBytes(IV_LENGTH);
- let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+ let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(config.enc), iv);
  let encrypted = cipher.update(text.toString());
 
  encrypted = Buffer.concat([encrypted, cipher.final()]);
@@ -20,7 +18,7 @@ function decrypt(text) {
  let textParts = text.split(':');
  let iv = Buffer.from(textParts.shift(), 'hex');
  let encryptedText = Buffer.from(textParts.join(':'), 'hex');
- let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+ let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(config.enc), iv);
  let decrypted = decipher.update(encryptedText);
 
  decrypted = Buffer.concat([decrypted, decipher.final()]);
